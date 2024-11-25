eae1f231ca1878ba6091b485c65ab00f2c53da69
viscaveronikat <visca@virtualspirit.me>
2024-11-25 16:29:44 +0700

    fix: follow - add transaction when follow/unfollow

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -2,19 +2,21 @@ class Api::V1::FollowsController < ApplicationController
   # POST /follows
   def create
     follow = Follow.new(follow_params)
-    if follow.save
-      render json: follow, status: :created
-    else
-      render json: { errors: follow.errors.full_messages }, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      if follow.save
+        render json: follow, status: :created
+      else
+        render json: { errors: follow.errors.full_messages }, status: :unprocessable_entity
+      end
     end
   end
 
   # DELETE /follows
-  def destroy
-    follow = Follow.find_by(user_id: params[:user_id], followed_user_id: params[:followed_user_id])
+  def unfollow
+    follow = Follow.find_by(user_id: params[:follow][:user_id], followed_user_id: params[:follow][:followed_user_id])
     if follow
       follow.destroy
-      render json: { message: "Unfollowed successfully" }, status: :ok
+      render json: { message: "Successfully Unfollow" }, status: :ok
     else
       render json: { error: "Follow relationship not found" }, status: :not_found
     end
