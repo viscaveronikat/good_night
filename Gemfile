3f258ed77d30f9c7bd62ecd8c94bd931569214f1
viscaveronikat <visca@virtualspirit.me>
2024-11-25 16:28:39 +0700

    feat: sidekiq configuration

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "solid_cache"
 gem "solid_queue"
 gem "solid_cable"
+gem 'redis'
+gem 'sidekiq'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+sidekiq_config = { url: ENV["REDIS_URL"] }
+sidekiq_config[:password] = ENV["REDIS_PASSWORD"] if ENV["REDIS_PASSWORD"].present?
+
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
+end
