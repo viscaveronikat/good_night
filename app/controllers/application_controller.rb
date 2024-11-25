fc268189d1546b6333bf883414e96549dc244e65
viscaveronikat <visca@virtualspirit.me>
2024-11-25 16:30:45 +0700

    feat: skip authenticity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  skip_before_action :verify_authenticity_token
   allow_browser versions: :modern
+
 end
